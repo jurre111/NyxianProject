9cc8638f058524262c5430704b17ab2443ead979
cr4zyengineer <fridakitten@pm.me>
2025-10-24 20:24:41 +0200

    Improved SynItems OfClangErrorWithString method

--- Nyxian/LindChain/Synpush/Synitem.m
+++ Nyxian/LindChain/Synpush/Synitem.m
@@ -35,33 +35,12 @@
 + (NSArray<Synitem*> *)OfClangErrorWithString:(NSString*)errorString
 {
     NSMutableArray<Synitem*> *issues = [[NSMutableArray alloc] init];
-    NSArray *errorLines = [errorString componentsSeparatedByString:@"\n"];
-    
-    for(NSString *line in errorLines)
-    {
-        NSArray *errorComponents = [line componentsSeparatedByString:@":"];
-        if([errorComponents count] >= 5)
-        {
-            Synitem *item = [[Synitem alloc] init];
-            item.line = [errorComponents[1] unsignedIntValue];
-            item.column = [errorComponents[2] unsignedIntValue];
-            item.type = [Synitem SynitemLevelOfClangLevel:errorComponents[3]];
-            
-            // MARK: Notes aint tracked anyways by Synpush and it must come as close to Synpush as possible
-            if(item.type == 0)
-                continue;
-            
-            NSRange messageRange = NSMakeRange(4, errorComponents.count - 4);
-            item.message = [[errorComponents subarrayWithRange:messageRange] componentsJoinedByString:@":"];
-            
-            [issues addObject:item];
-        }
-    }
-    
+    [self OfClangErrorWithString:errorString usingArray:&issues];
     return issues;
 }
 
-+ (void)OfClangErrorWithString:(NSString*)errorString usingArray:(NSMutableArray<Synitem*> **)issues
++ (void)OfClangErrorWithString:(NSString*)errorString
+                    usingArray:(NSMutableArray<Synitem*> **)issues
 {
     NSArray *errorLines = [errorString componentsSeparatedByString:@"\n"];
     
